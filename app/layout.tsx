87ddd291858c29249237c67d2dc95eb75d0e5985
YashwanthK <yashwanthkothakota@gmail.com>
2023-11-14 16:19:09 +0530

    complete header features

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import Header from "@/components/header";
 import "./globals.css";
 import { Poppins } from "next/font/google";
+import ActiveSectionContextProvider from "@/context/active-section-context";
 
 const poppins = Poppins({
   subsets: ["latin"],
@@ -20,14 +21,17 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
+    <html lang="en" className="!scroll-smooth">
       <body
         className={`${poppins.className} bg-teal-50 text-teal-950 relative h-[5000px] pt-28 sm:pt-36`}
       >
         <div className="bg-[#a7f3d0] absolute top-[-6rem] -z-10 right-[11rem] h-[31.25rem] w-[31.25rem] rounded-full blur-[10rem] sm:w-[68.75rem]"></div>
         <div className="bg-[#a5f3fc] absolute top-[-1rem] -z-10 left-[-35rem] h-[31.25rem] w-[50rem] rounded-full blur-[10rem] sm:w-[68.75rem] md:left-[-33rem] lg:left-[-28rem] xl:left-[-15rem] 2xl:left-[-5rem]"></div>
-        <Header />
-        {children}
+
+        <ActiveSectionContextProvider>
+          <Header />
+          {children}
+        </ActiveSectionContextProvider>
       </body>
     </html>
   );
