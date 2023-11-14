68cf1837acc5235ce53ff6050a78bde0e3c6e2c8
YashwanthK <yashwanthkothakota@gmail.com>
2023-11-14 06:28:10 +0530

    Add Intro section

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,13 @@
 import Header from "@/components/header";
 import "./globals.css";
-import { Inter } from "next/font/google";
+import { Poppins } from "next/font/google";
 
-const inter = Inter({ subsets: ["latin"] });
+const poppins = Poppins({
+  subsets: ["latin"],
+  display: "swap",
+  variable: "--font-poppins",
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+});
 
 export const metadata = {
   title: "Yashwanth | Personal Portfolio",
@@ -16,7 +21,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={`${inter.className} bg-teal-50 text-teal-950 relative`}>
+      <body
+        className={`${poppins.className} bg-teal-50 text-teal-950 relative h-[5000px] pt-28 sm:pt-36`}
+      >
         <div className="bg-[#a7f3d0] absolute top-[-6rem] -z-10 right-[11rem] h-[31.25rem] w-[31.25rem] rounded-full blur-[10rem] sm:w-[68.75rem]"></div>
         <div className="bg-[#a5f3fc] absolute top-[-1rem] -z-10 left-[-35rem] h-[31.25rem] w-[50rem] rounded-full blur-[10rem] sm:w-[68.75rem] md:left-[-33rem] lg:left-[-28rem] xl:left-[-15rem] 2xl:left-[-5rem]"></div>
         <Header />
