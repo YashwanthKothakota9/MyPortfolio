af3a2a7e029d9c6e9245c7713e9a297520f60684
YashwanthK <yashwanthkothakota@gmail.com>
2023-11-15 06:20:28 +0530

    Add footer

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import "./globals.css";
 import { Poppins } from "next/font/google";
 import ActiveSectionContextProvider from "@/context/active-section-context";
 import { Toaster } from "react-hot-toast";
+import Footer from "@/components/footer";
 
 const poppins = Poppins({
   subsets: ["latin"],
@@ -29,10 +30,12 @@ export default function RootLayout({
         <div className="bg-[#a7f3d0] absolute top-[-6rem] -z-10 right-[11rem] h-[31.25rem] w-[31.25rem] rounded-full blur-[10rem] sm:w-[68.75rem]"></div>
         <div className="bg-[#a5f3fc] absolute top-[-1rem] -z-10 left-[-35rem] h-[31.25rem] w-[50rem] rounded-full blur-[10rem] sm:w-[68.75rem] md:left-[-33rem] lg:left-[-28rem] xl:left-[-15rem] 2xl:left-[-5rem]"></div>
 
+        <Toaster position="top-right" />
+
         <ActiveSectionContextProvider>
           <Header />
           {children}
-          <Toaster position="top-right" />
+          <Footer />
         </ActiveSectionContextProvider>
       </body>
     </html>
